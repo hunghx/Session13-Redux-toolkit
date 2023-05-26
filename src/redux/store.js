16b1f6d67c3ed4990c88bb6e22a1a2f00004ebe6
hunghx <hung18061999hung@gmailcom>
2023-05-26 20:54:25 +0700

    commit 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
-import { combineReducers, createStore } from "@reduxjs/toolkit";
+import { combineReducers, createStore,configureStore } from "@reduxjs/toolkit";
 import { todoSlice } from "./reducers/todoSlice";
 import { editSlice } from "./reducers/editSlice";
 
-export const store = createStore(combineReducers({todos:todoSlice.reducer,editTask:editSlice.reducer}))
+// export const store = createStore(combineReducers({todos:todoSlice.reducer,editTask:editSlice.reducer}))
+export const store = configureStore({
+    reducer: {
+        todos: todoSlice.reducer,
+        editTask: editSlice.reducer
+    }
+})
